82220c863998e30ccd5aab3afae41c6ba5c019e4
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2026-02-25 11:12:33 +0100

    konstruktor létrehozása

--- student.py
+++ student.py
@@ -1,9 +1,11 @@
 class Student:
-    name = ""
-    age = 0
-    height = 0
-    gender = ""
-    score = 0
+
+    def __init__(self, name_, age_, height_, gender_):
+        self.name = name_
+        self.age = age_
+        self.height = height_
+        self.gender = gender_
+        self.score = 0
 
     def learn(self, test):
         self.score += test
@@ -11,19 +13,16 @@ class Student:
     def __str__(self):
         return (f'Hello, my name is: {self.name}, {self.age}, {self.height}, {self.gender}, {self.score}')
 
-lajos = Student()
-anna = Student()
-print(lajos.height)
-lajos.name = "Lajos"
-lajos.age = 13
-lajos.height = 178
-lajos.gender = "fiu"
+lajos = Student("Lajos", 18, 178, "fiu")
+anna = Student("Anna", 15, 170, "lany")
+
+
 print(lajos.height)
 print(lajos.name)
 print(lajos.score)
 lajos.learn(10)
 lajos.learn(10)
-print(lajos.score)
-
 print(lajos)
+print(lajos.score)
 print(anna)
+print(anna.score)
