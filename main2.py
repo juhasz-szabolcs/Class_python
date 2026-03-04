4081413fa58edf608a45f6f77059f22717ec41b4
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2026-03-04 09:36:21 +0100

    autók listában kezelése
    random sebességnövelés; Átlag életkor; Legidősebb autó

--- main2.py
+++ main2.py
@@ -1,17 +1,58 @@
 from auto import Auto
+import random
 
 auto1 = Auto("Toyota", "Corolla", 2015)
-auto2 = Auto("Ford", "Focus", 2018)
+auto2 = Auto("Ford", "Focus", 2002)
+auto3 = Auto("Audi", "E-Tron GT", 2021)
+auto4 = Auto("Ford", "Mustang", 2005)
+auto5 = Auto("Ford", "Mustang", 2002)
 
-print(auto1)
-# print(auto1.marka)
-print(auto2)
+# 2. Bejárás for ciklussal
+autok = [auto1, auto2, auto3, auto4, auto5]
+for auto in autok:
+    print(auto)
 
-auto1.gyorsit(150)
-print(auto1)
-auto1.gyorsit(150)
-print(auto1)
-auto1.fekez(100)
-print(auto1)
-auto1.fekez(105)
-print(auto1)
+# 3. Gyorsítás minden autóra
+print("\nGyorsít\n")
+for auto in autok:
+    auto.gyorsit(random.randint(30,150))
+    print(auto)
+
+# 4. Egyszerű statisztika – átlagéletkor
+autok_szama = len(autok)
+ossz_eletkor = 0
+for auto in autok:
+    kor = 2026 - auto.gyartasi_ev
+    ossz_eletkor += kor
+
+atlag_eletkor = ossz_eletkor / autok_szama
+print(f"Az autók átlagéletkora: {atlag_eletkor} év")
+
+# 5. Plusz (haladóknak): legöregebb autó
+# A version: FAVÁGÓ
+legidosebb_auto = None
+legidosebb_auto_kora = 0
+
+for auto in autok:
+    kor = 2026 - auto.gyartasi_ev
+    if kor > legidosebb_auto_kora:
+        legidosebb_auto_kora = kor
+        legidosebb_auto = auto
+
+print(f"A legidősebb autó kora: {legidosebb_auto_kora}, adatai: {legidosebb_auto}")
+
+# B version: indexof
+autok_kora = []
+for auto in autok:
+    autok_kora.append(2026 - auto.gyartasi_ev)
+
+legidosebb_auto_indexe = autok_kora.index(max(autok_kora))
+print(f"A legidősebb autó kora: {max(autok_kora)}, adatai: {autok[legidosebb_auto_indexe]}")
+
+# C version:
+gyartasi_evek = [auto.gyartasi_ev for auto in autok]
+
+for auto in autok:
+    if auto.gyartasi_ev == min(gyartasi_evek):
+        print(f"A legidősebb autó adatai: {auto}___{2026-auto.gyartasi_ev}")
+        print(f"A legidősebb autó: {auto.marka} {auto.tipus}")
